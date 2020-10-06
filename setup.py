239f37c8eb1945aac6082c16d0b93e8173cd8360
Eli Bendersky <eliben@gmail.com>
2020-10-06 08:11:37 -0700

    Complete placeholder setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="elftools",
+    version="0.0.1",
+    author="Eli Bendersky",
+    author_email="eliben@gmail.com"
+    description="Placeholder for pyelftools",
+    long_description="You want 'pip install pyelftools' instead",
+    long_description_content_type="text/markdown",
+    url="https://github.com/eliben/pyelftools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
