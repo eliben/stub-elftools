091fe9c98727456ac2d61712e9f0c1d90670a463
Eli Bendersky <eliben@gmail.com>
2020-10-06 08:26:21 -0700

    Clarify description
    Updates https://github.com/eliben/pyelftools/issues/334

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 import setuptools
 
+FULL_DESCRIPTION = r'''\
+This is not an actual project; you're probably looking for `pyelftools`
+instead. To install `pyelftools` run:
+
+    pip install pyelftools
+'''
+
 setuptools.setup(
     name="elftools",
     version="0.0.1",
     author="Eli Bendersky",
     author_email="eliben@gmail.com",
     description="Placeholder for pyelftools",
-    long_description="You want 'pip install pyelftools' instead",
+    long_description=FULL_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/eliben/pyelftools",
     packages=setuptools.find_packages(),
@@ -15,5 +22,4 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
 )
